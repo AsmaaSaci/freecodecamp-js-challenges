e79f73ad6d2ca99bd943f26e6b150923b7f2d8cf
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-08 09:21:27 +0100

    Create stellar-classification.js

---
+++ stellar-classification.js
@@ -0,0 +1,9 @@
+function classification(temp) {
+    if (temp >= 30000) return "O";
+    if (temp >= 10000) return "B";
+    if (temp >= 7500) return "A";
+    if (temp >= 6000) return "F";
+    if (temp >= 5200) return "G";
+    if (temp >= 3700) return "K";
+    return "M";
+}
